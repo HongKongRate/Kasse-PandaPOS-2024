29b4a085eb5415e110ec498e903b9341c11f2175
HongKongRate <pangsiufan888@gmail.com>
2023-09-30 01:07:45 +0200

    1

--- Frm_Tischplan.vb
+++ Frm_Tischplan.vb
@@ -1,17 +1,27 @@
 ﻿Imports TheArtOfDevHtmlRenderer.Adapters.Entities
 
 Public Class Frm_Tischplan
-    Dim rs As New Resizer
-    Private Sub Frm_Tischplan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'rs.FindAllControls(Me)
-    End Sub
+   Dim rs As New Resizer
+   Private Sub Frm_Tischplan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      Dim screenWidth As Integer = Screen.PrimaryScreen.Bounds.Width
+      Dim screenHeight As Integer = Screen.PrimaryScreen.Bounds.Height
+      'Sets the scaling mode
+      Me.AutoScaleMode = AutoScaleMode.Dpi
+      ''Enables appearance settings
+      'Me.gridControl1.DpiAware = True
+      'Dim Bildschirm = New Frm_Tischplan()
+      Me.Height = screenHeight
+      Me.Width = screenWidth
+      Me.StartPosition = FormStartPosition.CenterScreen
+      'rs.FindAllControls(Me)
+   End Sub
 
-    Private Sub Frm_Tischplan_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
-        'rs.ResizeAllControls(Me)
-    End Sub
+   Private Sub Frm_Tischplan_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+      'rs.ResizeAllControls(Me)
+   End Sub
 
-    Private Sub Frm_Tischplan_ClientSizeChanged(sender As Object, e As EventArgs) Handles Me.ClientSizeChanged
-        rs.FindAllControls(Me)
-        rs.ResizeAllControls(Me)
-    End Sub
+   Private Sub Frm_Tischplan_ClientSizeChanged(sender As Object, e As EventArgs) Handles MyBase.ClientSizeChanged
+      'rs.FindAllControls(Me)
+      'rs.ResizeAllControls(Me)
+   End Sub
 End Class
